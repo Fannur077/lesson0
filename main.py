cb9c8b3701f335d07f01dda2245bfaeb182a8ec3
Fannur077 <fannur.zaripov@mail.ru>
2024-08-21 19:33:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-print("1st program")
-print(9 ** 0.5 *5)
-print("2nd program")
-print(9.99 > 9.98 and 1000 !=1000.1)
-print("3rd program")
-print(2 * 2 + 2)
-print(2 * (2 + 2))
-print((2 * 2 + 2) == (2 * (2 + 2)))
-print("4th program")
-print('123.456')
-print((int(float( '123.456' ) * 10 ) % 10))
+from tkinter.scrolledtext import example
+
+example = 'Топинамбур'
+print(example [0])
+print(example [-1])
+print(example [5:])
+print(example[::-1])
+print(example[1:10:2])
