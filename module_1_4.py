547a09b27718d9627e54c2b06e2e19d129f662d6
Fannur077 <fannur.zaripov@mail.ru>
2024-08-22 09:44:24 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите слово: ')
+print(len(my_string))
+print(my_string .upper()) #в верхнем регистре
+print(my_string .lower()) #в нижнем регистре
+print(my_string .replace(' ','')) #удалив все пробелы
+print(my_string[0])
+print(my_string[-1])
+
